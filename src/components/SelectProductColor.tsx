15f76de5a7938edbcaf89552e3e8d5f51e5276d1
Pike1868 <luis_14mendoza@protonmail.com>
2024-05-07 17:56:54 -0400

    Added amount and color selection functionality to product page

--- src/components/SelectProductColor.tsx
+++ src/components/SelectProductColor.tsx
@@ -1,7 +1,26 @@
 
 
-export default function SelectProductColor() {
+type SelectProductColorProps = {
+  colors:string[],
+  productColor:string,
+  setProductColor:React.Dispatch<React.SetStateAction<string>>,
+}
+
+export default function SelectProductColor({colors, productColor, setProductColor}:SelectProductColorProps) {
   return (
-    <div>SelectProductColor</div>
+    <div className="mt-6">
+      <h4 className="font-medium tracking-wider capitalize text-md">colors</h4>
+      <div className="mt-2">
+        {colors.map((color)=>{
+          return <button 
+                   key={color} 
+                   type="button" 
+                   className={`rounded-full w-6 h-6 border-2 m-1 ${color === productColor && "border-primary"}`}
+                   style={{backgroundColor:color}}
+                   onClick={()=>setProductColor(color)}
+                 ></button>
+        })}
+      </div>
+    </div>
   )
 }
--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -1,11 +1,10 @@
-import { useLoaderData, Link } from "react-router-dom"
+import { useLoaderData, Link , type LoaderFunction } from "react-router-dom"
 import { customFetch, formatAsDollars, type SingleProductResponse } from "@/utils"
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
-import { type LoaderFunction } from "react-router-dom"
-
 import { SelectProductAmount, SelectProductColor } from "@/components"
+import { Mode } from "@/components/SelectProductAmount"
 
 export const loader:LoaderFunction = async({params}):Promise<SingleProductResponse> =>{
   const response = await customFetch<SingleProductResponse>(`/products/${params.id}`);
@@ -25,6 +24,39 @@ export default function singleProduct() {
   }
 
   return (
-    <h1 className="text-4xl">Single Product</h1>
+    <section>
+      <div className="flex items-center h-6 gap-x-2">
+        <Button asChild variant="link" size="sm">
+          <Link to="/">Home</Link>
+        </Button>
+        <Separator orientation="vertical"/>
+        <Button asChild variant="link" size="sm">
+          <Link to="/products">Products</Link>
+        </Button>
+      </div>
+
+      {/**PRODUCT */}
+      <div className="grid mt-6 gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+        {/** PRODUCT IMAGE - FIRST COLUMN */}
+        <img src={image} alt={title} className="object-cover rounded-lg w-96 h-96 lg:w-full"/>
+        {/**PRODUCT INFO - SECOND COLUMN */}
+        <div>
+          <h1 className="text-3xl font-bold capitalize">{title}</h1>
+          <h4 className="mt-2 text-xl">{company}</h4>
+          <p className="inline-block p-2 mt-3 font-bold rounded-md text-md bg-muted" >
+            {dollarsAmount}
+          </p>
+          <p className="mt-6 leading-8">{description}</p>
+          {/**COLORS */}
+          <SelectProductColor colors={colors} productColor={productColor} setProductColor={setProductColor}/>
+          {/**AMOUNT */}
+          <SelectProductAmount mode={Mode.SingleProduct} amount={amount} setAmount={setAmount}/>
+          {/**CART */}
+          <Button size="lg" className="mt-10 capitalize" onClick={addToCart}>
+            Add to cart
+          </Button>
+        </div>
+      </div>
+    </section>
   )
 }
